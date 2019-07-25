c6e75546c55d4e078abbd028fb9ecdab063ed0f0
shdown <shdownnine@gmail.com>
2019-07-25 15:50:11 +0300

    src/lib_llvm.cr: support for BasicBlock equality comparison

--- src/lib_llvm.cr
+++ src/lib_llvm.cr
@@ -1,14 +1,14 @@
 require "./lib_llvm_c"
 
 module LibLLVM
-    def self.slurp_string(pmsg : LibC::Char*)
+    def self.slurp_string (pmsg : LibC::Char*)
         msg = String.new(pmsg)
         LibLLVM_C.dispose_message(pmsg)
         return msg
     end
 
     class IrBuffer
-        def initialize(@value : LibLLVM_C::MemoryBufferRef)
+        def initialize (@value : LibLLVM_C::MemoryBufferRef)
         end
 
         def to_unsafe
@@ -21,7 +21,7 @@ module LibLLVM
     end
 
     class BasicBlock
-        def initialize(@value : LibLLVM_C::BasicBlockRef)
+        def initialize (@value : LibLLVM_C::BasicBlockRef)
         end
 
         def to_unsafe
@@ -39,10 +39,12 @@ module LibLLVM
         def last_instruction
             return LibLLVM_C.get_last_instruction(@value)
         end
+
+        def_equals @value
     end
 
     class Function
-        def initialize(@value : LibLLVM_C::ValueRef)
+        def initialize (@value : LibLLVM_C::ValueRef)
         end
 
         def to_unsafe
@@ -64,7 +66,7 @@ module LibLLVM
     end
 
     class IrModule
-        def initialize(@value : LibLLVM_C::ModuleRef)
+        def initialize (@value : LibLLVM_C::ModuleRef)
         end
 
         def to_unsafe
@@ -84,14 +86,14 @@ module LibLLVM
         end
     end
 
-    def self.buffer_from_file(path : String)
+    def self.buffer_from_file (path : String)
         if LibLLVM_C.create_memory_buffer_with_contents_of_file(path, out pbuf, out pmsg) != 0
             raise "Cannot open bitcode file: " + slurp_string(pmsg)
         end
         return IrBuffer.new(pbuf)
     end
 
-    def self.module_from_buffer(buf : IrBuffer)
+    def self.module_from_buffer (buf : IrBuffer)
         raise "Cannot parse bitcode" unless
             LibLLVM_C.parse_bitcode2(buf, out pmodule) == 0
         return IrModule.new(pmodule)
